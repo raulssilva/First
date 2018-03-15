af220ee367e64cf2f637aa67f31c5c4b274205e8
Raul Silveira Silva <raulmacintosh@imd.local>
2018-03-15 10:38:52 -0300

    Classe Pergunta

---
+++ First/src/Montador.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Montador {
+	private Pergunta[] perguntas = new Pergunta[5];
+	private Random gerador = new Random();
+	
+	public Montador() {
+		int[] perguntasLidas = new int[5];
+		for (int i = 0; i < 5; i++) {
+			int index = gerador.nextInt(11);
+			
+			
+		}
+	}
+}
