cfa9d59ec0f6395a788d7d9896266ecf4c5c98a2
Débora Costa <emilli.costa#hotmail.com>
2018-03-18 14:11:39 -0300

    add as perguntas, a classe do jogador e a classe de manipuação de arquivos

---
+++ First/src/Jogador.java
@@ -0,0 +1,35 @@
+
+public class Jogador {
+	private int pontuacao;
+	private int id;
+	
+	public Jogador() {
+		id=1;
+		pontuacao = 0;
+	}
+	
+	public Jogador(int id_) {
+		id = id_;
+		pontuacao =0;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id_) {
+		id = id_;
+	}
+	
+	public int getPontuacao() {
+		return pontuacao;
+	}
+	
+	public void setPontuacao(int pont) {
+		pontuacao = pont;
+	}
+	
+	public void addPontuacao(int pont) {
+		pontuacao += pont;
+	}
+}
---
+++ First/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Montador m = new Montador();
+		//String path = "Perguntas/pergunta1.txt";
+        //ManipuladorArquivo.leitor(path);
+
+	}
+
+}
